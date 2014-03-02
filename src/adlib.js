573235fc6135d3dda8603db881a6d5b12451b0d0
joshbeam <joshua.a.beam@gmail.com>
2014-03-02 12:53:28 -0500

    added documentation in comments
    minified as well

--- src/adlib.js
+++ src/adlib.js
@@ -436,12 +436,33 @@ joshua.a.beam@gmail.com
 		splice: emptyArray.splice,
 
 		/*
-		
-			Handle:
-				_('div').filter(1) ==> returns the DIV element with an index of 1 in relation to its original matched set
-				_('div').filter( _('.center') ) ==> returns all DIV elements with class 'center'
-				_('div').filter( function(el) { return el.get('index') > 0 } ) ==> (next line)
-					returns DIV elements with an index greater than 0 in relation to its original matched set
+			.filter(index)
+			.filter(AdLib instance)
+			.filter(callback)
+			
+			Filters elements of an AdLib instance by returning a new AdLib instance.
+			
+			Only one argument can be passed in.
+			
+			If the argument is a number:
+				
+				e.g. _('div').filter(1)
+				
+				It will return a new AdLib object of the element with the index corresponding
+				to the argument, in relation the instance's original matched set.
+				
+			If the argument is an AdLib instance:
+			
+				e.g. _('div').filter( _('.center') )
+				
+				It will return a new AdLib object of elements that match the passed in argument object.
+				
+			If the argument is a function:
+				
+				e.g. _('div').filter( function(el) { return el.get('index') > 0 } )
+				
+				It will return a new AdLib object with elements that are equal to the
+				return value of the passed in function.
 		*/
 		
 		filter: function() {
@@ -482,6 +503,43 @@ joshua.a.beam@gmail.com
 			
 			return _(result);
 		},
+		
+		/*
+			.get(property)
+			.get(type, property)
+			
+			Returns a value from the first element in the matched set.
+			
+			Only one or two arguments can be passed in.
+		
+			If one argument is passed:
+			
+				e.g. _('div').get('color')
+			
+				'text' ==> gets the text
+				'tag' ==> gets the tag
+				'html' ==> gets innerHTML
+				'value' ==> gets value for form elements
+				'index' ==> gets index relative to matched set
+				'states' ==> gets the 'states' object
+				
+				If none of the above are passed, it will try to find the argument string
+				inside document.documentElement.style or a styleAliases object.
+				
+				If it doesn't find the argument string in styles, it will try to
+				find an attribute matching the argument string.
+				
+			If two arguments are passed:
+			
+				e.g. _('div').get('attr','data-foo')
+			
+				'attr' , attribute ==> gets the value of an attribute
+				'style' , style ==> gets the style of an element
+				'state' , state ==> gets the custom set state of an element
+				
+			The AdLib instance IS NOT returned for chaining, because a value is returned.
+		*/
+		
 		get: function() {
 			var el = this[0],
 				args = arguments,
@@ -524,7 +582,66 @@ joshua.a.beam@gmail.com
 				}[type];				
 			}
 		},
-		set: function(type,prop,val) {
+		
+		/*
+			.set(object)
+			.set(property, value)
+			.set(type, property, value)
+			
+			Sets something for every element in the matched set and returns the
+			original AdLib instance for method chaining.
+			
+			Only 1, 2, or 3 arguments can be passed in.
+			
+			If one argument is passed:
+				
+				e.g. _('div').set({
+					style: {
+						color: 'red'
+					},
+					attr: {
+						'data-foo': 'bar'
+					}
+				});
+				
+				It must be an object of one or more objects.  The nested objects can
+				be called 'style' or 'attr'.
+				
+				The nested 'style' object can contain key-value pairs of style properties
+				and values.
+			
+				The nested 'attr' object can contian key-value pairs of attribute names
+				and values.
+				
+			If two arguments are passed:
+			
+				e.g. _('div').set('color','red')
+				
+				'text' , text ==> sets the text
+				'html' , html ==> sets the innerHTML
+				'class' , class ==> sets, adds, removes, or toggles a class
+					_('div').set('class','foo') ==> overwrite the class
+					_('div').set('class','add:foo bar') ==> adds two classes, 'foo' and 'bar'
+					_('div').set('class','remove:bar') ==> removes class 'bar'
+					_('div').set('class','toggle:foo') ==> toggles class 'foo'
+					
+				If none of those argument strings were passed in, it will try to set
+				a style, if the first argument matches a property in document.documentElement.style
+				or a styleAliases object.
+				
+				If it can't find the first argument in styles, it try to set an attribute with the
+				name of the first argument to the value of the second argument.
+				
+			If three arguments are passed:
+			
+				e.g. _('div').set('style','background-color','red')
+				
+				'state' , state , boolean ==> sets a 'state' of an object to true or false
+				'style' , property , value ==> sets a given style property to the value
+				'attr' , property , value ==> sets a given attribute to the value
+		*/
+		
+		set: function() {
 			var args = arguments,
 				len = args.length,
 				object,
@@ -555,21 +672,21 @@ joshua.a.beam@gmail.com
 
 					}
 				} else if (len===2) {
-					type = args[0];
-					prop = args[1];
+					prop = args[0];
+					val = args[1];
 
-					switch(type) {
+					switch(prop) {
 						case 'text':
-							setText(el,prop);
+							setText(el,val);
 							break;
 						case 'html':
-							setHTML(el,prop);
+							setHTML(el,val);
 							break;
 						case 'class':
-							setClass(el,prop);
+							setClass(el,val);
 							break;
 						default:
-							setStyle(el,type,prop) || el.setAttribute(type,prop);
+							setStyle(el,prop,val) || el.setAttribute(prop,val);
 							break;
 					}
 				} else if (len===3) {
@@ -594,6 +711,38 @@ joshua.a.beam@gmail.com
 			return this;
 
 		},
+		
+		/*
+			.has(string)
+			.has(AdLib instance)
+			.has(type, property)
+			
+			Returns either a number or a boolean value for the first matched element in the set.
+			
+			Only one or two arguments can be passed in.
+			
+			If one arguments in passed in:
+			
+				e.g. _('div').has('children')
+			
+				'children' ==> returns the number of child elements
+				'text' ==> return true or false, depending on whether the element has textNode elements
+				
+				If the argument in an AdLib instance, it will return true or false, depending on
+				whether or not the AdLib instance calling the method contains the AdLib instance
+				that is passed in as the argument.
+				
+				If the argument contains < or >, it assumes the user wants to see if the caller
+				contains a certain HTML string.  It will return true or false, depending on whether
+				the innerHTML contains the argument string.
+				
+				If the argument is a text string, it will return true or false, depending on whether
+				the innerText contains the argument string.
+			
+			The AdLib instance IS NOT returned for chaining, because a value is returned.
+		
+		*/
+		
 		has: function() {
 			var el,
 				args = arguments,
@@ -652,6 +801,24 @@ joshua.a.beam@gmail.com
 			}
 			// what about div.has('attr','id','someId') ?
 		},
+		
+		/*
+			.is(state)
+			Returns either a number or a boolean value for the first matched element in the set.
+			
+			Only one argument can be passed in.
+			
+			If one arguments in passed in:
+			
+				e.g. _('div').is('open')
+			
+				- Returns true if the state was set to true.
+				- Returns false if the state was set to false, or if the state was never set.
+			
+			The AdLib instance IS NOT returned for chaining, because a value is returned.			
+			
+		*/
+		
 		is: function() {
 			var el = this[0],
 				state = arguments[0];
@@ -666,6 +833,34 @@ joshua.a.beam@gmail.com
 					
 				}
 		},
+		
+		/*
+			.each(function)
+			
+			Iterates through each element in the matched set and calls a function in the context
+			of the currently iterated element.  The original AdLib instance is returned for method
+			chaining.
+			
+			Only one argument can be passed in.
+			
+			If one argument is passed:
+				
+				e.g. var colors = [];
+				
+				_('div').each(function(element, index) {
+				
+					colors.push( element.get('color') );
+					
+					console.log(index)
+				
+				});
+				
+				The currently iterated element as an AdLib object is passed in as
+				the first argument to the callback function that the user provides the method.
+				The index of the currently iterated element in relation to the caller's matched set 
+				is passed in as the second argument.
+		*/
+		
 		each: function(fn) {
 			var el = this;
 			
@@ -675,6 +870,44 @@ joshua.a.beam@gmail.com
 			
 			return this;
 		},
+		
+		/*
+			.on(eventType, handler)
+			.on(eventType, delegatedElement, handler)
+			
+			Attaches an event handler to every element in the matched set.
+			The original AdLib instance is returned for method chaining.
+			
+			Only two or three arguments can be passed in.
+			
+			If two argument are passed:
+				
+				e.g. function log(e) { console.log(e.target }
+				
+				_('div').on('click', log);
+				
+				This method uses addEventListener if available, or attachEvent for IE<9
+				
+				It is recommended that the user passes in a named function (as opposed to
+				an anonymous function), because only named functions can be later detached
+				from the elements using the .off() method.
+			
+			If three arguments are passed:
+			
+				e.g. function log(e) { console.log(e.target) }
+				
+				_('body').on('click','div',log)
+				
+				This method attaches an event listener to a parent element, and
+				the corresponding handler only fires if the event target is
+				the second argument (which is a selector string that is internally
+				turned into an AdLib instance).
+				
+				At this time, named handlers cannot be detached from elements using
+				.off() if they were attached using the argument pattern for event
+				delegation.	
+		*/
+		
 		on: function() {
 			var len = arguments.length,
 				eventType = arguments[0],
@@ -719,6 +952,30 @@ joshua.a.beam@gmail.com
 			
 			return this;
 		},
+		
+		/*
+			.off(eventType, handler)
+			
+			Detaches an event handler from every element in the matched set.
+			The original AdLib instance is returned for method chaining.
+			
+			Only two arguments can be passed in.
+			
+			If two argument are passed:
+				
+				e.g. _('div').off('click',log)
+				
+				It detaches an event listener using removeEventListener if available,
+				or detachEvent for IE<9.
+				
+				This method cannot be used to detach all event listeners of a specific
+				type (for example, _('div').off('click') does nothing).
+				
+				Currently, this method cannot detached listeners that were attached
+				using the event delegation argument pattern in .on().
+
+		*/
+		
 		off: function(eventType, handler) {
 			
 			forEach(this, function(el) {
